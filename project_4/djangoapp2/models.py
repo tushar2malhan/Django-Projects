0d885d1973d6445ece6027745919f87b15991939
Tushar <tusharmalhan@gmail.com>
2021-12-18 23:13:15 +0530

    final changes for all projects"
    "

--- project_4/djangoapp2/models.py
+++ project_4/djangoapp2/models.py
@@ -21,8 +21,8 @@ class Mammal(models.Model):
 class Bird(models.Model):
     name = models.CharField(max_length=100,primary_key=True)
     species = models.CharField(max_length=100,null=True, blank=True)
-    food = models.CharField(max_length=100,null=True)
-    last_feed = models.DateField(null=True,blank=True)
+    food =models.CharField(max_length=100,null=True)
+    last_feed=models.DateField(null=True,blank=True)
     
     def __str__(self):
         return f'{self.name , self.species}'
